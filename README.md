5728b2aca382b6d90f34f4bf216987b22f4b1581
Alexander_Fnug <alexander.fnug-soltani@1508.dk>
2024-02-01 18:54:03 +0100

    Virker nogenlunde

---
+++ README.md
@@ -0,0 +1,4 @@
+Her er den deployede side:
+
+Jeg gik i gang med en algoritme for sjov, men stopper nu hehe
+
--- script.js
+++ script.js
@@ -1,20 +1,59 @@
 document.querySelector("#start").addEventListener("click", start);
 
+let guess = 50;
+let higherCounter = 1;
+
 function start() {
   event.preventDefault();
   console.log("JS loaded");
-  firstGuess();
+  newguess(guess);
 }
 
-function firstGuess() {
+function newguess(guess) {
   event.preventDefault();
+  //remove previous buttons but keep previous guesses
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.remove();
+  });
   const list = document.querySelector("#guesslist");
   //    // insert this list item before the end of the list with the number 50 and three buttons higher, lower, correct with appropriate ids. use insertAdjacentHTML
   list.insertAdjacentHTML(
     "beforeend",
-    "<li>50<button id='higher'>Higher</button><button id='lower'>Lower</button><button id='correct'>Correct</button></li>"
+    `<li>${guess}<button id='higher'>Higher</button><button id='lower'>Lower</button><button id='correct'>Correct</button></li>`
   );
   document.querySelector("#higher").addEventListener("click", higher);
   document.querySelector("#lower").addEventListener("click", lower);
   document.querySelector("#correct").addEventListener("click", correct);
+}
+
+function higher() {
+  event.preventDefault();
+  console.log("higher");
+  guess = 50
+  higherCounter = higherCounter *2;
+  console.log(higherCounter);
+  if (guess % 2 == 0) {
+   guess = Math.round((guess + guess / higherCounter), 0)
+    newguess(guess);
+  } else {
+    guess = Math.round((guess + (guess - 1) / higherCounter), 0);
+    newguess(guess);
+  }
+}
+
+function lower() {
+  event.preventDefault();
+  console.log("lower");
+  if (guess % 2 == 0) {
+    guess = guess / 2;
+    newguess(guess);
+  } else {
+    guess = (guess + 1) / 2;
+    newguess(guess);
+  }
+}
+
+function correct() {
+  console.log("correct");
 }
